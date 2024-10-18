e25eb3ab26e0ae996a8ab01dc2f79b2668b65439
eyal71006 <eyal71006@gmail.com>
2024-10-18 12:14:02 +0300

    set filter params based on available apartments only

--- src/helpers/getFilterParams.js
+++ src/helpers/getFilterParams.js
@@ -3,12 +3,13 @@ const getFilterParams = (apartments) => {
     let minPrice = apartments[0]?.price;
     let locations = [];
     apartments?.forEach(apartment => {
+        if (!apartment.available) return;
         if (apartment.price > maxPrice) maxPrice = apartment.price;
         if (apartment.price < minPrice) minPrice = apartment.price;
         if (!locations.includes(apartment.address.state.toLowerCase())) locations.push(apartment.address.state.toLowerCase());
         if (!locations.includes(apartment.address.country.toLowerCase())) locations.push(apartment.address.country.toLowerCase());
         if (!locations.includes(apartment.address.city.toLowerCase())) locations.push(apartment.address.city.toLowerCase());
-    });    
+    });
     return { maxPrice, minPrice, locations };
 };
 
